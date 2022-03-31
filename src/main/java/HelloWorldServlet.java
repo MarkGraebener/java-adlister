0e6a4c090eb4b6ecc29cd682be28e475e8f7b9ce
MarkGraebener <mark.w.graebener@gmail.com>
2022-03-31 10:59:59 -0500

    test

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,7 +7,8 @@ import javax.servlet.http.*;
 public class HelloWorldServlet extends HttpServlet {
 
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h1>Hello, World!</h1>");
